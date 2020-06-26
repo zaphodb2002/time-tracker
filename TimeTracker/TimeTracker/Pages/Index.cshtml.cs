6b2ae59844dc9d0c9bc10d7bb28d0813d3f24947
Joshua Lindhardt <zaphodb2002@gmail.com>
2020-06-26 12:31:04 -0700

    Details page implemented

--- TimeTracker/TimeTracker/Pages/Index.cshtml.cs
+++ TimeTracker/TimeTracker/Pages/Index.cshtml.cs
@@ -17,9 +17,9 @@ namespace TimeTracker.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return RedirectToPage("/Route");
         }
     }
 }
---
+++ TimeTracker/TimeTracker/Pages/Locations/Detail.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TimeTracker.Data;
+using TimeTracker.Data.Entities;
+
+namespace TimeTracker.Pages
+{
+    public class DetailModel : PageModel
+    {
+        public Location Location { get; set; }
+        public ILocationData LocationData { get; }
+
+        public DetailModel(ILocationData locationData)
+        {
+            this.LocationData = locationData;
+        }
+
+        public IActionResult OnGet(uint locationId)
+        {
+            Location = LocationData.Get(locationId);
+            if (Location == null)
+            {
+                return BadRequest("No Such Location Found");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
